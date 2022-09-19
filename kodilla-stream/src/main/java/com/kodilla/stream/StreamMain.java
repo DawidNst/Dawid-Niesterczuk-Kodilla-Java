15bad7d0b233a68f78d31bc11caf159831222b8e
Deiwo <dawidniesterczuk@gmail.com>
2022-09-19 11:35:01 +0200

    7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 
 public class StreamMain
@@ -14,6 +15,10 @@ public class StreamMain
         poemBeautifier.beatufiy("Lazyday", (param)-> "3) " + param+param.toUpperCase() + param );
         poemBeautifier.beatufiy("Programmer", (param)-> "4) If you can: \"" + param + "\" anyone can !" );
         poemBeautifier.beatufiy("ABCDEFGH",(param)->"5)"+param+param.toLowerCase());
-        System.out.println(":-)");
+        System.out.println(":-)"+"\n");
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
+
     }
+
 }
