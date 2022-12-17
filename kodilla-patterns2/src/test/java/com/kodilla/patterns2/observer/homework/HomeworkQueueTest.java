a577db9af92be11218ef15c30944cb9cd2e76b84
DawidNst <dawidniesterczuk@gmail.com>
2022-12-17 15:57:37 +0100

    24,4 zadanie

---
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/observer/homework/HomeworkQueueTest.java
@@ -0,0 +1,40 @@
+package com.kodilla.patterns2.observer.homework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HomeworkQueueTest {
+
+    @Test
+    public void testUpdateHomeworkList(){
+        //Given
+        Mentor janusz = new Mentor("Janusz");
+        Mentor andrzej = new Mentor("Andrzej");
+
+        Student dawid = new Student("dawid");
+        Student kuba = new Student("kuba");
+        Student irena = new Student("irena");
+        Student halina = new Student("halina");
+
+        dawid.registerObserver(janusz);
+        kuba.registerObserver(janusz);
+        irena.registerObserver(janusz);
+        halina.registerObserver(andrzej);
+
+        //When
+        dawid.sendHomework("task1");
+        dawid.sendHomework("task2");
+        kuba.sendHomework("task1");
+        kuba.sendHomework("task2");
+        kuba.sendHomework("task3");
+        irena.sendHomework("task1");
+        halina.sendHomework("task1");
+        halina.sendHomework("task2");
+        halina.sendHomework("task3");
+
+        //Then
+        assertEquals(3, andrzej.getUpdateCount());
+
+    }
+}
